97f2bf99eb5427bd633ccad59e80cbc3230a3ba9
BTS611 <gusmao.yasmin@escola.pr.gov.br>
2025-10-17 11:45:16 -0300

    upidate

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const pokemon_img = document.getElementById("pkimg")
 const pokemon_form = document.getElementById("form")
 const pokemon_imput = document.getElementById("imput")
 
-const pokemon_prev = document.getElementById("prev")
-const 
+const pokemon_prev = document.getElementById("bnt=prev")
+const pokemon_next = document.getElementById("bnt-next")
